053791479fad24603de75a8fe8c553f4e3d353b4
Stanly Johnson <stanly-johnson@users.noreply.github.com>
2015-09-06 08:11:08 +0530

    comments added

--- motor.h
+++ motor.h
@@ -1,12 +1,17 @@
 //use only when motor functions are required
 //contains only port initialisation and delay function
 
+/* The connection to motor is as follows 
+    LEFT_MOTOR_FORWARD : PIN2
+    LEFT_MOTOR_BACKWARD : PIN1
+    RIGHT_MOTOR_FORWARD : PIN4
+    RIGHT_MOTOR_BAVKWARD : PIN3
+ */
 //CHANGE VALUE HERE AS REQUIRED
 #define CPORT PORTD
 
-
 void front(int del){
- 
+ //--IMPORTANT-- verify motor connection matches with the program
  CPORT = 0b00001010;
  _delay_ms(del);
 
